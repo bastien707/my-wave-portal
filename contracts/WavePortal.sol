b48721a8dee42fed6d094be3b067c7c105d1053d
bastien707 <bastien.facqueur@efrei.net>
2022-04-12 15:58:41 -0700

    wave function added

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -5,7 +5,22 @@ pragma solidity ^0.8.4;
 import "hardhat/console.sol";
 
 contract WavePortal{
+    //totalWaves is a state variable => stored permanently in contract storage
+    uint256 totalWaves;
+
     constructor() {
         console.log("Yo yo, I'm a contract and I am smart");
+    }
+
+    //we know who is msg.sender because in order to call a smart contract you 
+    //need to be connected to a wallet
+    function wave() public {
+        totalWaves +=1;
+        console.log("%s has waved!", msg.sender);
+    }
+
+    function getTotalWaves() public view returns (uint256) {
+        console.log("We have %d total waves!", totalWaves);
+        return totalWaves;
     }
 }
